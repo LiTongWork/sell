89337fd6b8af21ee70dddb2c5bc6e2d5aadecd33
LiTongWork <2272701464@qq.com>
2019-01-18 17:29:38 +0800

    仿饿了么APP商家页面

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      i am tab
+    </div>
+    <div class="content">
+      i am content
+    </div>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+  import vHeader from './components/header/header'
+export default {
+  name: 'App',
+  components: {
+    'v-header': vHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-header">
+    我来组成头部
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-header'
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
